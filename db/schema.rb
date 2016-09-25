b5d85d7038d41f005f6a907dc355d3dcfd0f2add
Lee Richmond <lrichmond1@bloomberg.net>
2016-09-25 15:58:44 -0400

    Check in pg schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20160925161842) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friendships", force: :cascade do |t|
     t.integer  "friendee_id"
     t.integer  "friender_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["friendee_id"], name: "index_friendships_on_friendee_id"
-    t.index ["friender_id"], name: "index_friendships_on_friender_id"
+    t.index ["friendee_id"], name: "index_friendships_on_friendee_id", using: :btree
+    t.index ["friender_id"], name: "index_friendships_on_friender_id", using: :btree
   end
 
   create_table "messages", force: :cascade do |t|
@@ -27,8 +30,8 @@ ActiveRecord::Schema.define(version: 20160925161842) do
     t.text     "content"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["receiver_id"], name: "index_messages_on_receiver_id"
-    t.index ["sender_id"], name: "index_messages_on_sender_id"
+    t.index ["receiver_id"], name: "index_messages_on_receiver_id", using: :btree
+    t.index ["sender_id"], name: "index_messages_on_sender_id", using: :btree
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -37,8 +40,8 @@ ActiveRecord::Schema.define(version: 20160925161842) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_tweets_on_author_id"
-    t.index ["parent_id"], name: "index_tweets_on_parent_id"
+    t.index ["author_id"], name: "index_tweets_on_author_id", using: :btree
+    t.index ["parent_id"], name: "index_tweets_on_parent_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
