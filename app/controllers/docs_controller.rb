1939232befcd069a0a75e7b67059f4303db1a292
Lee Richmond <lrichmond1@bloomberg.net>
2016-09-25 15:59:37 -0400

    Temp disable docs

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -5,11 +5,13 @@ class DocsController < ApplicationController
     key :swagger, '2.0'
   end
 
-  jsonapi_resource '/api/tweets', tags: ['tweets']
-  jsonapi_resource '/api/users',
-    tags: ['users'],
-    only: [:index, :show]
-  jsonapi_resource '/api/messages',
-    tags: ['messages'],
-    only: [:index, :show]
+  unless Rails.env.production?
+    jsonapi_resource '/api/tweets', tags: ['tweets']
+    jsonapi_resource '/api/users',
+      tags: ['users'],
+      only: [:index, :show]
+    jsonapi_resource '/api/messages',
+      tags: ['messages'],
+      only: [:index, :show]
+  end
 end
