ac4ab4165146f61e5ed6c684dfb2692ebaf8369b
Lee Richmond <lrichmond1@bloomberg.net>
2016-09-25 16:04:16 -0400

    Reenable swagger

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -5,13 +5,11 @@ class DocsController < ApplicationController
     key :swagger, '2.0'
   end
 
-  unless Rails.env.production?
-    jsonapi_resource '/api/tweets', tags: ['tweets']
-    jsonapi_resource '/api/users',
-      tags: ['users'],
-      only: [:index, :show]
-    jsonapi_resource '/api/messages',
-      tags: ['messages'],
-      only: [:index, :show]
-  end
+  jsonapi_resource '/api/tweets', tags: ['tweets']
+  jsonapi_resource '/api/users',
+    tags: ['users'],
+    only: [:index, :show]
+  jsonapi_resource '/api/messages',
+    tags: ['messages'],
+    only: [:index, :show]
 end
